ccd01fb9a6a1a830c20c310c67aa2249d866b3af
duc-lap <rumbonaruson@gmail.com>
2024-02-11 17:09:51 +0700

    Classes problem: add Testcase in description to Program file.

--- Classes_StudentTeacher/Program.cs
+++ Classes_StudentTeacher/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Classes_StudentTeacher
 {
-    internal class Program
+    internal class StudentAndTeacherTest
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World");
+            Person person = new Person();
+            Student student = new Student();
+            Teacher teacher = new Teacher();
+
+            Console.WriteLine("Person says hello:");
+            person.Greet();
+            Console.WriteLine();
+            Console.WriteLine("Student is greeting:");
+            student.SetAge(21);
+            student.Greet();
+            student.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("Teacher says hello:");
+            teacher.SetAge(30);
+            teacher.Greet();
+            teacher.Explain();
         }
     }
 }
